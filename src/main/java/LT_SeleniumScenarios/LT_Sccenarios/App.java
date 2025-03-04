563da9e0cad6bc8138af0c777afc708e0b19b8ad
mghubeOutlook <monika_ghube@outlook.com>
2025-03-04 10:03:02 +0530

    selenium project
    push commit selenium

---
+++ src/main/java/LT_SeleniumScenarios/LT_Sccenarios/App.java
@@ -0,0 +1,13 @@
+package LT_SeleniumScenarios.LT_Sccenarios;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
